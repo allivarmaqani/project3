e2c6f7d82475c0499fe6dc2c4b08f0a337d3191d
allivarmaqani <allivarmaqani@gmail.com>
2025-02-20 08:51:45 -0800

    add style to project and then remove it from app.vue and past it into styles.css

--- chat/src/App.vue
+++ chat/src/App.vue
@@ -3,8 +3,13 @@
 </template>
 
 <script>
-
+import db from './db';
 export default {
+  setup(){
+    return{
+
+    }
+  }
 }
 </script>
 
---
+++ chat/src/db.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+
+const config ={
+    apiKey: "AIzaSyAeFE0IfXQyEJICWDV01FTps9oPo6SMUHo",
+    authDomain: "firevuechat-59d82.firebaseapp.com",
+    projectId: "firevuechat-59d82",
+    storageBucket: "firevuechat-59d82.firebasestorage.app",
+    messagingSenderId: "1097853031981",
+    appId: "1:1097853031981:web:ca965aca9db96a074e4aa4"
+}
+
+const db =firebase.initializeApp(config);
+export default db;
