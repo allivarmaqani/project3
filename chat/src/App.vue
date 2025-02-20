b82683a830fc42d975b18af1f9a8a03d57d5377b
allivarmaqani <allivarmaqani@gmail.com>
2025-02-20 14:43:13 -0800

    add font , add style,add script , make project rtl,make project ot persian

--- chat/src/App.vue
+++ chat/src/App.vue
@@ -1,97 +1,120 @@
 <template>
-<div class="view login" v-if="state.username === ''|| state.username === null">
-  <form class="login-form" @submit.prevent="Login">
- <div class="form-inner">
-  <h1>Login to firechat</h1>
-    <label for="username">username</label>
-    <input type="text" v-model="inputUsername" placeholder="Please enter your username" required />
-    <input type="submit" value="Login" />
- </div>
-  </form>
-</div>
-<div class="view chat" v-else>
- <header>
-  <button class="logout">logout</button>
-  <h1>welocme,{{ state.username }}</h1>
- </header>
- <section class="chat-box">
- <div v-for="message in state.messages" :key="message.key" :class="(message.username == state.username ? 'message current-user' :'message')">
-<div class="message-inner">
-  <div class="username">{{ message.username }}</div>
-  <div class="content">{{ message.content}}</div>
-</div>
- </div>
-</section>
- <footer>
-  <form @submit.prevent="SendMessage">
-    <input type="text" v-model="inputMessage" placeholder="write a message..."/>
-    <input type="submit" value="Send" >
-  </form>
- </footer>
-</div>
+  <div class="view login" v-if="state.username === '' || state.username === null">
+    <form class="login-form" @submit.prevent="Login">
+      <div class="form-inner">
+        <h1>ورود به صفحه چت</h1>
+        <label for="username">نام کاربری</label>
+        <input 
+          type="text" 
+          v-model="inputUsername" 
+          placeholder="لطفا نام کاربری خود را وارد کنید" />
+        <input 
+          type="submit" 
+          value="Login" />
+      </div>
+    </form>
+  </div>
+  
+  <div class="view chat" v-else>
+    <header>
+      <button class="logout" @click="Logout">ورود</button>
+      <h1>خوش آمدید, {{ state.username }}</h1>
+    </header>
+    
+    <section class="chat-box">
+      <div 
+        v-for="message in state.messages" 
+        :key="message.key" 
+        :class="(message.username == state.username ? 'message current-user' : 'message')">
+        <div class="message-inner">
+          <div class="username">{{ message.username }}</div>
+          <div class="content">{{ message.content }}</div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <form @submit.prevent="SendMessage">
+        <input 
+          type="text" 
+          v-model="inputMessage" 
+          placeholder="پیام" />
+        <input 
+          type="submit" 
+          value="ارسال" />
+      </form>
+    </footer>
+  </div>
 </template>
 
 <script>
-import { reactive,onMounted,ref } from 'vue';
+import { reactive, onMounted, ref } from 'vue';
 import db from './db';
 
 export default {
-  setup(){
-    const inputUsername =ref("");
-    const inputMessage =ref("");
+  setup () {
+    const inputUsername = ref("");
+    const inputMessage = ref("");
 
-    const state =reactive({
-      username:"",
-      messages:[]
-    })
+    const state = reactive({
+      username: "",
+      messages: []
+    });
 
-
-    const Login =()=>{
-      if(inputUsername.value !="" || inputUsername.value !=null){
-        state.username = inputUsername.value
-inputUsername.value ="";
+    const Login = () => {
+      if (inputUsername.value != "" || inputUsername.value != null) {
+        state.username = inputUsername.value;
+        inputUsername.value = "";
       }
     }
 
-    const SendMessage =() =>{
-      const messageRef = db.database().ref("messages");
+    const Logout = () => {
+      state.username = "";
+    }
 
-      if(inputMessage.value ==="" || inputMessage.value ===null){
-      return;
+    const SendMessage = () => {
+      const messagesRef = db.database().ref("messages");
+
+      if (inputMessage.value === "" || inputMessage.value === null) {
+        return;
       }
+
       const message = {
-        username:state.username,
+        username: state.username,
         content: inputMessage.value
       }
-      messageRef.push(message);
-      inputMessage.value ="";
+
+      messagesRef.push(message);
+      inputMessage.value = "";
     }
 
-    onMounted(()=>{
-      const messageRef = db.database().ref("messages");
+    onMounted(() => {
+      const messagesRef = db.database().ref("messages");
 
-      messageRef.on('value',snapshot =>{
+      messagesRef.on('value', snapshot => {
         const data = snapshot.val();
         let messages = [];
 
-      Object.keys(data).forEach(key =>{
-        messages.push({
-          id:key,
-          username: data[key].username,
-          content: data[key].content,
+        Object.keys(data).forEach(key => {
+          messages.push({
+            id: key,
+            username: data[key].username,
+            content: data[key].content
+          });
         });
+
+        state.messages = messages;
       });
-      state.messages = messages;
-      })
-    })
-    return{
- inputUsername,
- Login,
- state,
- inputMessage,
- SendMessage
+    });
+
+    return {
+      inputUsername,
+      Login,
+      state,
+      inputMessage,
+      SendMessage,
+      Logout
     }
   }
 }
 </script>
-
--- chat/src/db.js
+++ chat/src/db.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const config = {
-    apiKey: "AIzaSyAeFE0IfXQyEJICWDV01FTps9oPo6SMUHo",
-    authDomain: "firevuechat-59d82.firebaseapp.com",
-    projectId: "firevuechat-59d82",
-    storageBucket: "firevuechat-59d82.firebasestorage.app",
-    messagingSenderId: "1097853031981",
-    appId: "1:1097853031981:web:ca965aca9db96a074e4aa4"
-}
+    apiKey: "AIzaSyCfTZ202FjI4tmawDbizlPsBRcu3eCSqyI",
+    authDomain: "chat-da008.firebaseapp.com",
+    projectId: "chat-da008",
+    storageBucket: "chat-da008.firebasestorage.app",
+    messagingSenderId: "405077229883",
+    appId: "1:405077229883:web:2fcdef5ae3f4836db8f71e"
+  };
 
 const db = firebase.initializeApp(config);
 export default db;
