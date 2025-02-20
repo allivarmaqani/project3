96ed84536884a1460b989651b3fa9e5a9a3bdcb4
allivarmaqani <allivarmaqani@gmail.com>
2025-02-20 10:08:20 -0800

    add js code & html to project

--- chat/src/App.vue
+++ chat/src/App.vue
@@ -1,13 +1,43 @@
 <template>
-<h1>hellow world</h1>
+<div class="view login" v-if="state.username === ''|| state.username === null">
+  <form class="login-form" @submit.prevent="Login">
+ <div class="form-inner">
+  <h1>Login to firechat</h1>
+    <label for="username">username</label>
+    <input type="text" v-model="inputUsername" placeholder="Please enter your username" required />
+    <input type="submit" value="Login" />
+ </div>
+  </form>
+</div>
+<div class="view chat" v-else>
+  <h1>Chat View</h1>
+</div>
 </template>
 
 <script>
-import db from './db';
+import { reactive,onMounted,ref } from 'vue';
+
 export default {
   setup(){
-    return{
+    const inputUsername =ref("");
 
+
+    const state =reactive({
+      username:"",
+      messages:[]
+    })
+
+
+    const Login =()=>{
+      if(inputUsername.value !="" || inputUsername.value !=null){
+        state.username = inputUsername.value
+inputUsername.value ="";
+      }
+    }
+    return{
+ inputUsername,
+ Login,
+ state
     }
   }
 }
