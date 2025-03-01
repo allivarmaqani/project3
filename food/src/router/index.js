9c206b69885c9389aede83de6facc34a237b0736
allivarmaqani <allivarmaqani@gmail.com>
2025-03-01 02:21:30 -0800

    add html and style to project

--- food/src/router/index.js
+++ food/src/router/index.js
@@ -1,28 +1,42 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 import Home from '../view/Home.vue'
-import MealList from '../view/MealList.vue';
 import MealsByName from '../view/MealsByName.vue';
 import MealsByLetter from '../view/MealsByLetter.vue';
 import MealsByIngredient from '../view/MealsByIngredient.vue';
-const routes =[{
+import DefaultLayout from '../components/DefaultLayout.vue';
+import GuestLayout from '../components/GuestLayout.vue';
+const routes =[
+  {
     path:'/',
-    name:'home',
-    component:Home,
+    component:DefaultLayout,
+    children:[{
+      path:'/',
+      name:'home',
+      component:Home,
+  },
+  {
+    path:'/by-name/:name?',
+    name:'byName',
+    component:MealsByName,
+  },{
+    path:'/by-letter/:letter?',
+    name:'byLetter',
+    component:MealsByLetter,
+  },{
+    path:'/by-ingredient/:ingredient?',
+    name:'byIngredient',
+    component:MealsByIngredient,
+  },
+ ],
 },
 {
-  path:'/by-name/:name?',
-  name:'byName',
-  component:MealsByName,
-},{
-  path:'/by-letter/:letter?',
-  name:'byLetter',
-  component:MealsByLetter,
-},{
-  path:'/by-ingredient/:ingredient?',
-  name:'byIngredient',
-  component:MealsByIngredient,
-},
-]
+  path: '/guest',
+  component:GuestLayout,
+}
+];
+
+
+
 const router = createRouter({
   history: createMemoryHistory(),
   routes,
