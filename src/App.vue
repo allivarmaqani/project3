75ddc809897a08884a883bd7b1034c261c8f8551
allivarmaqani <allivarmaqani@gmail.com>
2025-03-29 17:19:59 -0700

    add html css

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
 <template>
 <router-view/>
 </template>
-
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
-  const instruments = ref([])
-  async function getInstruments() {
-    const { data } = await supabase.from('instruments').select()
-    instruments.value = data
-  }
-  onMounted(() => {
-    getInstruments()
-  })
-  </script>
